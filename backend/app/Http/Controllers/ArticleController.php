db1f88ad4a6ad2189d89a0a45396c00ee1c6ec0c
r39u1 <andrew.regul@mail.ru>
2022-07-07 00:55:54 +0500

    Delete create show methods from ArticleController

--- backend/app/Http/Controllers/ArticleController.php
+++ backend/app/Http/Controllers/ArticleController.php
@@ -19,16 +19,6 @@ class ArticleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -54,17 +44,6 @@ class ArticleController extends Controller
     {
         $article = Article::find($id);
         return response()->json($article);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
