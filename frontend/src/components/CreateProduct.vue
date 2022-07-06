ca0042a052987b53ce167ce4d0225cba7aee57bd
r39u1 <andrew.regul@mail.ru>
2022-07-06 15:10:27 +0500

    Create CreateArticle component

---
+++ frontend/src/components/CreateProduct.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h3>Create Article</h3>
+        <div>
+            <form @submit.prevent="addArticle">
+                <label>Title</label>
+                <input type="text" v-model="article.title">
+                <label>Text</label>
+                <textarea v-model="article.text"></textarea>
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_ENDPOINT
+
+export default {
+    data() {
+        return {
+            article: {}
+        }
+    },
+    methods: {
+        addArticle() {
+            axios.post('/api/article', this.product)
+                .then(response => (
+                    this.$router.push({ name: 'home' })
+                ))
+                .catch(err => console.log(err))
+                .finally(() => this.loading = false)
+        }
+        
+    }
+}
+</script>
